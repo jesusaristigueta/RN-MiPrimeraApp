14fe8d9a87a8706f6ab55d375441072dd011889e
Jesus Aristigueta <jesusaristigueta@gmail.com>
2024-02-25 18:32:49 -0400

    Codigo base

---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,20 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+export const HolaMundoScreen = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 45,
+          textAlign: 'center',
+        }}>
+        Hola Mundo!
+      </Text>
+    </View>
+  );
+};
